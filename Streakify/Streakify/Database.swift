1373328c79f7fdc9650d462cf1552f73b1df14c0
ratheearya <ratheearya@gmail.com>
2024-04-26 00:00:09 -0700

    Created Database

---
+++ Streakify/Streakify/Database.swift
@@ -0,0 +1,27 @@
+//
+//  Database.swift
+//  Streakify
+//
+//  Created by Arya on 4/25/24.
+//
+
+//This DataBase is used to store User LogIn information, including 
+import Foundation
+import SwiftData
+
+class Database:Identifiable {
+    var id: String
+    var name: String
+    var username: String
+    var email: String
+    var password: String
+    
+    init(name: String, username: String, email: String, password: String){
+        self.id = UUID().uuidString
+        self.name = name
+        self.username = username
+        self.email = email
+        self.password = password
+    }
+    
+}
