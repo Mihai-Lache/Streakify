b3af1ccfc04f81c38ee27c10344bf612ec79e096
Mihai-Lache <141415155+Mihai-Lache@users.noreply.github.com>
2024-05-13 17:40:50 -0700

    progress bar stuff
    It can reset now.
    
    Made some aesthetic changes to the buttons, ---- could change back but im leaving it for now, Gonna checj in with prathik

--- Streakify/Streakify/ViewControllerGroup/MainPage.swift
+++ Streakify/Streakify/ViewControllerGroup/MainPage.swift
@@ -3,13 +3,13 @@ import SwiftUI
 struct MainPageView: View {
     @State private var habits: [Habit] = []
     @State private var showingAddHabit = false
-    @State private var showingSettings = false  // State to control navigation to the SettingsView
+    @State private var showingSettings = false
     let username: String = "User"
     let backgroundColor = Color(red: 11 / 255, green: 37 / 255, blue: 64 / 255)
     let progressTrackColor = Color.white.opacity(0.3)
     let progressColor = Color.green
 
-    @Environment(\.presentationMode) var presentationMode  // For dismissing the view
+    @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
         NavigationView {
@@ -27,7 +27,7 @@ struct MainPageView: View {
                         Spacer()
 
                         Button("Sign Out") {
-                            presentationMode.wrappedValue.dismiss()  // Dismisses the current view, simulating "sign out"
+                            presentationMode.wrappedValue.dismiss()
                         }
                         .foregroundColor(.white)
                         .padding(10)
@@ -38,13 +38,14 @@ struct MainPageView: View {
                     List {
                         ForEach(habits) { habit in
                             HabitRow(habit: habit, habits: $habits)
+                                .listRowBackground(backgroundColor)
                         }
                         .onDelete(perform: deleteHabits)
                     }
                     .listStyle(PlainListStyle())
+                    .background(backgroundColor)
 
                     HStack {
-                        // Settings Button
                         Button(action: {
                             showingSettings.toggle()
                         }) {
@@ -57,13 +58,14 @@ struct MainPageView: View {
                             .foregroundColor(.white)
                         }
                         .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
                         .sheet(isPresented: $showingSettings) {
                             SettingsView()
                         }
 
                         Spacer()
 
-                        // Add Habit Button
                         Button(action: {
                             showingAddHabit.toggle()
                         }) {
@@ -76,10 +78,13 @@ struct MainPageView: View {
                             .foregroundColor(.white)
                         }
                         .padding()
+                        .background(Color.green)
+                        .cornerRadius(10)
                         .sheet(isPresented: $showingAddHabit) {
                             AddHabitView(habits: $habits)
                         }
                     }
+                    .padding(.horizontal)
                 }
             }
             .navigationBarHidden(true)
@@ -91,58 +96,70 @@ struct MainPageView: View {
     }
 }
 
+
 struct HabitRow: View {
     var habit: Habit
     @Binding var habits: [Habit]
+    
+    @State private var showingDetail = false
 
     var body: some View {
-        HStack(spacing: 8) { // Decreased spacing between buttons
-            // Completion Toggle Button
+        HStack(spacing: 8) {
             Button(action: {
                 if let index = habits.firstIndex(where: { $0.id == habit.id }) {
-                    habits[index].isCompleted.toggle()  // Toggle completion status
+                    habits[index].isCompleted.toggle()
                     if habits[index].isCompleted {
-                        habits[index].streakCount = habits[index].totalDuration  // Ensure progress reaches 100%
+                        habits[index].streakCount = habits[index].totalDuration
+                    } else {
+                        habits[index].streakCount = 0  // Reset streak count
                     }
                 }
-
             }) {
                 Image(systemName: habit.isCompleted ? "checkmark.circle.fill" : "circle")
                     .foregroundColor(habit.isCompleted ? .green : .gray)
                     .imageScale(.large)
             }
 
-            Text(habit.name)
-                .foregroundColor(.white)
-                .strikethrough(habit.isCompleted, color: .white)  // Apply strikethrough if completed
-
-            // Visual progress representation
-            ZStack(alignment: .leading) {
-                Capsule().fill(Color.white.opacity(0.3))
-                    .frame(width: 140, height: 8)
-                Capsule().fill(Color.green)
-                    .frame(width: CGFloat(habit.progress) * 140, height: 8)
-                
-                // Display "Completed" or percentage in the middle of the bar
-                Text(habit.isCompleted ? "Completed" : "\(Int(habit.progressPercentage))%")
+            VStack(alignment: .leading) {
+                Text(habit.name)
                     .foregroundColor(.white)
-                    .font(.caption)
-                    .fontWeight(.bold)
-                    .position(x: 70, y: 4) // Centered position for the percentage text
+                    .strikethrough(habit.isCompleted, color: .white)
+
+                ZStack(alignment: .leading) {
+                    Capsule().fill(Color.white.opacity(0.3))
+                        .frame(width: 140, height: 8)
+                    Capsule().fill(Color.green)
+                        .frame(width: CGFloat(habit.progress) * 140, height: 8)
+
+                    Text(habit.isCompleted ? "Completed" : "\(Int(habit.progressPercentage))%")
+                        .foregroundColor(.white)
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .position(x: 70, y: 4)
+                }
+                .frame(width: 140, height: 8)
+                .cornerRadius(4)
             }
-            .frame(width: 140, height: 8)
-            .cornerRadius(4)
+            .onTapGesture {
+                showingDetail = true
+            }
+            .background(
+                NavigationLink(
+                    destination: HabitDetailView(habit: habit),
+                    isActive: $showingDetail,
+                    label: { EmptyView() }
+                )
+                .hidden()
+            )
 
             Spacer()
 
-            // Flame button with counter to signify streaks
-            // Flame button with counter to signify streaks
             Button(action: {
                 if let index = habits.firstIndex(where: { $0.id == habit.id }) {
                     if !habits[index].isCompleted && habits[index].streakCount < habits[index].totalDuration {
-                        habits[index].streakCount += 1  // Increment streak count
+                        habits[index].streakCount += 1
                         if habits[index].streakCount == habits[index].totalDuration {
-                            habits[index].isCompleted = true  // Automatically mark as completed if the total duration is reached
+                            habits[index].isCompleted = true
                         }
                     }
                 }
@@ -158,10 +175,8 @@ struct HabitRow: View {
                         .fontWeight(.bold)
                 }
             }
-
             .buttonStyle(PlainButtonStyle())
 
-            // Notification button
             Button(action: {
                 // Implement notification action
             }) {
@@ -172,7 +187,6 @@ struct HabitRow: View {
             }
             .buttonStyle(PlainButtonStyle())
 
-            // Delete button
             Button(action: {
                 if let index = habits.firstIndex(where: { $0.id == habit.id }) {
                     habits.remove(at: index)
@@ -185,12 +199,55 @@ struct HabitRow: View {
             }
             .buttonStyle(PlainButtonStyle())
         }
-        .listRowBackground(Color(red: 11 / 255, green: 37 / 255, blue: 64 / 255))
+        .padding(.vertical, 8)
+        .background(Color(red: 11 / 255, green: 37 / 255, blue: 64 / 255))
+        .cornerRadius(10)
     }
 }
+
+
 
 struct MainPageView_Previews: PreviewProvider {
     static var previews: some View {
         MainPageView()
+    }
+}
+
+
+
+struct HabitDetailView: View {
+    var habit: Habit
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(habit.name)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+
+            Text("Streak: \(habit.streakCount) / \(habit.totalDuration)")
+                .font(.title2)
+                .padding([.leading, .bottom])
+
+            ProgressView(value: habit.progress)
+                .progressViewStyle(LinearProgressViewStyle(tint: Color.green))
+                .padding([.leading, .trailing, .bottom])
+
+            Spacer()
+
+            Button(action: {
+                // Additional actions for the habit
+            }) {
+                Text("Additional Options")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+        }
+        .background(Color(red: 11 / 255, green: 37 / 255, blue: 64 / 255))
+        .foregroundColor(.white)
+        .edgesIgnoringSafeArea(.all)
     }
 }
