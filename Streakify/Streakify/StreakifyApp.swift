03157bd0e06c48d52b564cddabed46f08203c6bb
SrikarRani <95519867+SrikarRani@users.noreply.github.com>
2024-05-16 17:03:55 -0700

    Commiting to the branch
    Commit to merge

--- Streakify/Streakify/StreakifyApp.swift
+++ Streakify/Streakify/StreakifyApp.swift
@@ -5,18 +5,24 @@
 //
 
 import SwiftUI
-import SwiftData
 
 @main
-struct StreakifyApp: App
-{
-    var body: some Scene
-    {
-        WindowGroup
-        {
-            LoginView()
-            SignUpView()
+struct StreakifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
         }
-        
+    }
+}
+
+struct ContentView: View {
+    @State private var showLogin = true
+
+    var body: some View {
+        if showLogin {
+            LoginView(showLogin: $showLogin)
+        } else {
+            SignUpView(showLogin: $showLogin)
+        }
     }
 }
